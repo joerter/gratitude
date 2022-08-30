0e2513d6eb5b2354bccbbbfd8b8a673a8dc9cf2e
John Oerter <john@johnoerter.me>
2022-08-30 07:02:04 -0400

    add notes list

--- app/routes/notes/index.tsx
+++ app/routes/notes/index.tsx
@@ -1,12 +1,32 @@
-import { Link } from "@remix-run/react";
+import type { LoaderArgs } from "@remix-run/node";
+import { json } from "@remix-run/node";
+import { requireUserId } from "~/session.server";
+import { getNoteListItems } from "~/models/note.server";
+import { Link, useLoaderData } from "@remix-run/react";
+
+export async function loader({ request }: LoaderArgs) {
+  const userId = await requireUserId(request);
+  const noteListItems = await getNoteListItems({ userId });
+  return json({ noteListItems });
+}
 
 export default function NoteIndexPage() {
+  const data = useLoaderData<typeof loader>();
   return (
-    <p>
-      No note selected. Select a note on the left, or{" "}
-      <Link to="new" className="text-blue-500 underline">
-        create a new note.
-      </Link>
-    </p>
+    <div>
+      <p>
+        No note selected. Select a note below, or{" "}
+        <Link to="new" className="text-blue-500 underline">
+          create a new note.
+        </Link>
+      </p>
+      <ul>
+        {data.noteListItems.map((n, i) => (
+          <li key={i}>
+            <Link to={`${n.id}`}>{n.createdAt}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 }
